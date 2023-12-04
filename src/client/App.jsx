f0741c24bc125edf6c1dc0bd3fa31078a58bfada
Paula Mason <paulamason@Paulas-MacBook-Pro.local>
2023-12-04 12:14:52 -0500

    added frontend branch and some starter frontend code

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,9 +7,8 @@ function App() {
 
   return (
     <div className='App'>
-        <h1>Boilerplate</h1>
+        <h1>Ratamovie</h1>
         <img id='comp-img' src='./computer.png'></img>
-        <p>Replace the starter code in this template with something cool</p>
         <Login />
     </div>
   );
